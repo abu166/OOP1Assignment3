525d9a53365a7eff26b3d668f03c900b52c12577
ABukhassym <109275536+abu166@users.noreply.github.com>
2024-02-13 17:15:10 +0600

    Main.java

--- OOPAssignment3/src/Main.java
+++ OOPAssignment3/src/Main.java
@@ -12,33 +12,43 @@ public class Main {
 
         System.out.println("Welcome, " + name + "! You are at the University.");
 
-        System.out.println("Choose where you want to go:");
-        System.out.println("1. Library");
-        System.out.println("2. OpenSpace");
-        System.out.println("3. Dining Room");
-        int choice = scanner.nextInt();
+        System.out.println("Do you want to enter the University? (yes/no)");
+        String enterChoice = scanner.next();
 
-        switch (choice) {
-            case 1:
-                System.out.println("You are in the Library.");
-                break;
-            case 2:
-                System.out.println("You are in the OpenSpace.");
-                break;
-            case 3:
-                System.out.println("You are in the Dining Room.");
-                break;
-            default:
-                System.out.println("Invalid choice.");
-                break;
-        }
+        if (enterChoice.equalsIgnoreCase("yes")) {
+            System.out.println("You entered the University.");
 
-        System.out.println("Do you want to stay at the University and study? (yes/no)");
-        String stayChoice = scanner.next();
+            System.out.println("Choose where you want to go:");
+            System.out.println("1. Library");
+            System.out.println("2. OpenSpace");
+            System.out.println("3. Dining Room");
+            int choice = scanner.nextInt();
 
-        if (stayChoice.equalsIgnoreCase("yes")) {
-            System.out.println("You chose to stay and study.");
+            switch (choice) {
+                case 1:
+                    System.out.println("You are in the Library.");
+                    break;
+                case 2:
+                    System.out.println("You are in the OpenSpace.");
+                    break;
+                case 3:
+                    System.out.println("You are in the Dining Room.");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
+            }
+
+            System.out.println("Do you want to stay at the University and study? (yes/no)");
+            String stayChoice = scanner.next();
+
+            if (stayChoice.equalsIgnoreCase("yes")) {
+                System.out.println("You chose to stay and study.");
+            } else {
+                System.out.println("You left the University.");
+            }
         } else {
+            System.out.println("You chose not to enter the University.");
             System.out.println("You left the University.");
         }
     }
